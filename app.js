c28cb02558db0c2d367219310106b747ed349f9b
Mason Simon <masonsimon@gmail.com>
2012-03-01 14:12:04 -0800

    Started over with a simpler server. Somewhat unclean, but now has basic chat functionality.

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express.createServer()
+  , io = require('socket.io').listen(app);
+
+app.listen(80);
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection', function (socket) {
+
+  socket.emit('message', { who:'???', msg:'Howdy yall' });
+
+  socket.on('message', function (data) {
+    console.log(data);
+    io.sockets.emit('message', data);
+  });
+
+});
+
